cd3ce15d3fe741abc1fff0f945854dddb8e7bdb4
changchiyou <changchiyou@gmail.com>
2023-06-19 17:06:13 +0800

    test: update `test_OutputLogger.py`

--- tests/test_OutputLogger.py
+++ tests/test_OutputLogger.py
@@ -1,42 +1,66 @@
-import logging
 import contextlib
+import logging
 from pathlib import Path
 
-from haplog import instantiate_logger, OutputLogger
+from haplog import MultiProcessLogger, OutputLogger, worker_configurer
 
-LOGGER_NAME = 'test'
-MESSAGE = 'test'
+LOGGER_NAME = "test"
+MESSAGE = "test"
 
 
-def test_redirect(capsys):
-    instantiate_logger(LOGGER_NAME, level_console=logging.DEBUG)
+def test_redirect(capfd):
+    mpl = MultiProcessLogger(level_console=logging.DEBUG)
+    mpl.start()
+    worker_configurer(mpl.queue)
     with contextlib.redirect_stdout(
-            OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)):    # type: ignore
+        OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)  # type: ignore
+    ):  # type: ignore
         print(MESSAGE)
 
-    captured = capsys.readouterr()
-    assert f'DEBUG    {Path(__file__).name} - test_redirect() : {MESSAGE}' in captured.err
+    mpl.join()
+
+    captured = capfd.readouterr()
+    assert (
+        f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name} - test_redirect() : {MESSAGE}"
+        in captured.err
+    )
 
 
-def test_redirect_info_level(capsys):
-    instantiate_logger(LOGGER_NAME)
+def test_redirect_info_level(capfd):
+    mpl = MultiProcessLogger()
+    mpl.start()
+    worker_configurer(mpl.queue)
     with contextlib.redirect_stdout(
-            OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)):    # type: ignore
+        OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)  # type: ignore
+    ):  # type: ignore
         print(MESSAGE)
 
-    captured = capsys.readouterr()
-    assert f'DEBUG    {Path(__file__).name} - test_redirect() : {MESSAGE}' not in captured.err
+    mpl.join()
+
+    captured = capfd.readouterr()
+    assert (
+        f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name} - test_redirect() : {MESSAGE}"
+        not in captured.err
+    )
 
 
-def test_redirect_other_function(capsys):
-    instantiate_logger(LOGGER_NAME, level_console=logging.DEBUG)
+def test_redirect_other_function(capfd):
+    mpl = MultiProcessLogger(level_console=logging.DEBUG)
+    mpl.start()
+    worker_configurer(mpl.queue)
 
     def test():
         print(MESSAGE)
 
     with contextlib.redirect_stdout(
-            OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)):    # type: ignore
+        OutputLogger(logger_name=LOGGER_NAME, logging_level=logging.DEBUG)  # type: ignore
+    ):  # type: ignore
         test()
 
-    captured = capsys.readouterr()
-    assert f'DEBUG    {Path(__file__).name} - test() : {MESSAGE}' in captured.err
+    mpl.join()
+
+    captured = capfd.readouterr()
+    assert (
+        f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name} - test() : {MESSAGE}"
+        in captured.err
+    )
