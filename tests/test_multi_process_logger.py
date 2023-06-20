2da44230e63a9d6a43c5b6b9b5d3182bcdec17e7
changchiyou <changchiyou@gmail.com>
2023-06-21 00:15:49 +0800

    test: use `inspect.stack()[0][3]` to get current fucntion name instead of fixed string
    also slightly update the docstring of unit test

--- tests/test_multi_process_logger.py
+++ tests/test_multi_process_logger.py
@@ -1,4 +1,5 @@
 """Unit test for `haplog.MultiProcessLogger` via `pytest`."""
+import inspect
 import logging
 from pathlib import Path
 
@@ -11,8 +12,9 @@ LOGGER_NAME = "test"
 MESSAGE = "test"
 
 
-def test_console_default_info(capfd):
-    """Test the console logging with default level (INFO)."""
+# pylint: disable-next=invalid-name
+def test_console_default_info_single_process(capfd):
+    """(Single-process)Test the console logging with default level (INFO)."""
     mpl = MultiProcessLogger()
     mpl.start()
     worker_configurer(mpl.queue)
@@ -26,13 +28,14 @@ def test_console_default_info(capfd):
     captured = capfd.readouterr()
 
     assert (
-        f"INFO     [{LOGGER_NAME}] {Path(__file__).name} - test_console_default_info() : {MESSAGE}"
-        in captured.err
+        f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
+        f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
     )
 
 
-def test_console_info(capfd):
-    """Test the console logging with explicit INFO level."""
+# pylint: disable-next=invalid-name
+def test_console_info_single_process(capfd):
+    """(Single-process)Test the console logging with explicit INFO level."""
     mpl = MultiProcessLogger(level_console=logging.INFO)
     mpl.start()
     worker_configurer(mpl.queue)
@@ -46,13 +49,14 @@ def test_console_info(capfd):
     captured = capfd.readouterr()
 
     assert (
-        f"INFO     [{LOGGER_NAME}] {Path(__file__).name} - test_console_info() : {MESSAGE}"
-        in captured.err
+        f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
+        f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
     )
 
 
-def test_console_debug(capfd):
-    """Test the console logging with DEBUG level."""
+# pylint: disable-next=invalid-name
+def test_console_debug_single_process(capfd):
+    """(Single-process)Test the console logging with DEBUG level."""
     mpl = MultiProcessLogger(level_console=logging.DEBUG)
     mpl.start()
     worker_configurer(mpl.queue)
@@ -66,13 +70,18 @@ def test_console_debug(capfd):
     captured = capfd.readouterr()
 
     assert (
-        f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name} - test_console_debug() : {MESSAGE}"
-        in captured.err
+        f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
+        f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
     )
 
 
-def test_console_debug_info_level(capfd):
-    """Test the console logging with default level (INFO) but log with DEBUG and INFO level."""
+# pylint: disable-next=invalid-name
+def test_console_debug_info_level_single_process(capfd):
+    """
+    (Single-process)Test the project's ability to filter out messages based on `logging.INFO`
+    logging console levels with both DEBUG and INFO level logger, should only appear INFO's
+    message in console.
+    """
     mpl = MultiProcessLogger()
     mpl.start()
     worker_configurer(mpl.queue)
@@ -88,16 +97,21 @@ def test_console_debug_info_level(capfd):
 
     assert (
         f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
-        f" - test_console_debug_info_level() : {MESSAGE}" not in captured.err
+        f" - {inspect.stack()[0][3]}() : {MESSAGE}" not in captured.err
     )
     assert (
         f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
-        f" - test_console_debug_info_level() : {MESSAGE}" in captured.err
+        f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
     )
 
 
-def test_log(tmp_path, capfd):
-    """Test writing logs to a log file with both DEBUG level."""
+# pylint: disable-next=invalid-name
+def test_log_single_process(tmp_path, capfd):
+    """
+    (Single-process)Test writing logs to a log file based on both logs/console `logging.DEBUG`,
+    should appear ALL message from logger because `logging.DEBUG` doesn't filt any
+    message in default.
+    """
     mpl = MultiProcessLogger(log_path=tmp_path, level_console=logging.DEBUG)
     mpl.start()
     worker_configurer(mpl.queue)
@@ -116,22 +130,32 @@ def test_log(tmp_path, capfd):
         captured = capfd.readouterr()
 
         assert (
-            f"INFO     [{LOGGER_NAME}] {Path(__file__).name} - test_log() : {MESSAGE}"
-            in captured.err
+            f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
+        )
+        assert (
+            f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
         )
 
         assert (
-            f"INFO     [{LOGGER_NAME}] {Path(__file__).name} - test_log() : {MESSAGE}"
-            in contents
+            f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in contents
         )
         assert (
-            f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name} - test_log() : {MESSAGE}"
-            in contents
+            f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in contents
         )
 
 
-def test_console_log_diff_level(tmp_path, capfd):
-    """Test console logging and log file writing with different levels."""
+# pylint: disable-next=invalid-name
+def test_console_log_diff_level_single_process(tmp_path, capfd):
+    """
+    (Single-process)Test the project's ability to filter out messages based on `logging.INFO`
+    logging console levels and `logging.DEBUG` logs level with both DEBUG and INFO level logger,
+    should only appear INFO's message in console but contain both messages from DEBUG and INFO
+    level logger in log file.
+    """
     mpl = MultiProcessLogger(
         log_path=tmp_path,
         # default
@@ -156,19 +180,18 @@ def test_console_log_diff_level(tmp_path, capfd):
 
         assert (
             f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
-            f" - test_console_log_diff_level() : {MESSAGE}" in captured.err
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in captured.err
         )
-
         assert (
             f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
-            f" - test_console_log_diff_level() : {MESSAGE}" not in captured.err
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" not in captured.err
         )
 
         assert (
             f"INFO     [{LOGGER_NAME}] {Path(__file__).name}"
-            f" - test_console_log_diff_level() : {MESSAGE}" in contents
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in contents
         )
         assert (
             f"DEBUG    [{LOGGER_NAME}] {Path(__file__).name}"
-            f" - test_console_log_diff_level() : {MESSAGE}" in contents
+            f" - {inspect.stack()[0][3]}() : {MESSAGE}" in contents
         )
